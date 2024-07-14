5006cc95836a9f1a8afa503b80559002be719857
Thanh Duong <thanhduonghd114@gmail.com>
2024-07-14 17:34:21 +0300

    update: connected to open ai api and mongodb

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 
 import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js'; 
  
 dotenv.config(); 
 
@@ -10,13 +12,16 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
+
 app.get('/', async (req, res) => {
   res.send('Hello World!');
 });
 
 const startServer = () => {
   try {
-    connectDB(process.env.MONGO_URL);
+    connectDB(process.env.MONGODB_URL);
     console.log('Connected to MongoDB');
 
     const port = process.env.PORT || 8080;
---
+++ server/routes/dalleRoutes.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import OpenAI from 'openai';
+
+dotenv.config();
+
+
+const router = express.Router();
+
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+router.route('/').get((req, res) => 
+  res.send('Hello from DALL-E API')
+);
+
+export default router;
